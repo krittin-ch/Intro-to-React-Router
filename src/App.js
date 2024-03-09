a0bfeb92d851567d3bffdaafa2a5fd45c1c12182
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-03-09 07:27:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 // Components
 import Home from './pages/Home';
 import About from './pages/About'
+import Vans from './pages/Vans' 
 
 function App() {
   return (
     <BrowserRouter>
     <header>
-      <Link to={"/"}>#VANLIFE</Link>
+      <Link className="site-logo" to={"/"}>#VANLIFE</Link>
       <nav>
         <Link to={"/about"}>About</Link>
+        <Link to={"/vans"}>Vans</Link>
       </nav>
     </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
